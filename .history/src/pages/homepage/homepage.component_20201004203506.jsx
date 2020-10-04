e4313e72516fb6774b91abfd9b55c8432294822f
fulman <zhapar28@gmail.com>
2020-10-04 21:03:45 +0600

    separated homepage into smaller components, finished styling menu-items component

---
+++ .history/src/pages/homepage/homepage.component_20201004203506.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Diretory from "../../components/directory/directory.component";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return <div className="homepage"></div>;
+};
+
+export default HomePage;
