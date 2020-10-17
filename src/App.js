48e370c4c6a26816284c911b319cfccd237d0166
zhapar <zhapar28@gmail.com>
2020-10-17 23:19:52 +0600

    Finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -22,12 +22,15 @@ class App extends Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
             },
-          });
+            () => console.log(this.state.currentUser)
+          );
         });
       } else {
         this.setState({ currentUser: userAuth });
